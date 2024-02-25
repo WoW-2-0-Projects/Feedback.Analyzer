175bac09e34d22e38860e9d370615ffe69aa95fa
Botirjon <thedavlativich@gmail.com>
2024-02-25 15:48:34 +0500

    Add an asynchronous event bus subscriber

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/EventBus/Services/IAsyncEventBusSubscriber.cs
@@ -0,0 +1,9 @@
+namespace Feedback.Analyzer.Application.Common.EventBus.Services;
+
+/// <summary>
+/// Represents an interface for an asynchronous event bus subscriber.
+/// </summary>
+public interface IAsyncEventBusSubscriber
+{
+    
+}
