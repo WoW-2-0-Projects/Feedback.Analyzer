7f4f6d81659ea3dee487bcf2cad595ed9fa122f6
Bunyod <kbunyod3011@gmail.com>
2024-02-25 13:08:00 +0500

    Register persistence to middleware

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Api/Configurations/HostConfigurations.Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Feedback.Analyzer.Persistence.DataContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedback.Analyzer.Api.Configurations;
+
+public static partial class HostConfigurations
+{
+    private static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+    {
+        // register ef interceptors
+        
+        //register db context
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("FeedbackAnalyzer");
+        });
+
+        return builder;
+    }
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Api/Configurations/HostConfigurations.cs
@@ -0,0 +1,17 @@
+﻿namespace Feedback.Analyzer.Api.Configurations;
+
+public static partial class HostConfigurations
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddPersistence();
+        
+        return new(builder);
+    }
+    
+    public static async ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        return app;
+    }
+}
