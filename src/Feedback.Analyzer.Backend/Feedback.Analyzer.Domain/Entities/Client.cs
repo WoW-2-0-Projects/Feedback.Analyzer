e9e914ebba02f0cf0d715714fa944ee211423e3c
Bunyod <kbunyod3011@gmail.com>
2024-02-26 11:29:15 +0500

    Add Client entity and entity configuration

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Domain/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿using Feedback.Analyzer.Domain.Common.Entities;
+
+namespace Feedback.Analyzer.Domain.Entities;
+
+public class Client : AuditableEntity
+{
+    public string FirstName { get; set; } = default!;
+
+    public string LastName { get; set; } = default!;
+
+    public string Email { get; set; } = default!;
+
+    public string Password { get; set; } = default!;
+
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Configurations/ClientConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Feedback.Analyzer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Feedback.Analyzer.Persistence.Configurations;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder)
+    {
+        builder.Property(client => client.FirstName).HasMaxLength(256).IsRequired();
+        builder.Property(client => client.LastName).HasMaxLength(256).IsRequired();
+        builder.Property(client => client.Email).HasMaxLength(128).IsRequired();
+        builder.Property(client => client.Password).HasMaxLength(128).IsRequired();
+    }
+}
