3992c2134a9ea95490d37b1210cbab4e29be88d2
Botirjon <thedavlativich@gmail.com>
2024-02-26 09:59:25 +0500

    Remove unnecessary services and settings

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/EventBus/Services/IAsyncEventBusSubscriber.cs
+++
@@ -1,9 +0,0 @@
-namespace Feedback.Analyzer.Application.Common.EventBus.Services;
-
-/// <summary>
-/// Represents an interface for an asynchronous event bus subscriber.
-/// </summary>
-public interface IAsyncEventBusSubscriber
-{
-    
-}
