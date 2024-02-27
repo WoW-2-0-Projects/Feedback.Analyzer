1cd30f55e360d50375bbb945d8226e1710044ad4
Bunyod <kbunyod3011@gmail.com>
2024-02-27 10:15:05 +0500

    Add FilterPagination

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Domain/Common/Queries/QueryOptions.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Domain/Common/Queries/QueryOptions.cs
@@ -3,11 +3,11 @@
 /// <summary>
 /// Represents a set of options to configure data querying behavior.
 /// </summary>
-public struct QueryOptions
+public struct QueryOptions()
 {
     /// <summary>
     /// Gets or sets a value indicating whether change tracking should be disabled for query results. 
     /// Disabling change tracking can potentially improve performance.
     /// </summary>
-    public bool AsNoTracking { get; set; }
+    public bool AsNoTracking { get; set; } = false;
 }
