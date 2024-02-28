a8ec88dfe4ef15bf63e2782b13b530e7e3b43378
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-02-28 13:35:46 +0500

    Add customer feedback query

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/CustomerFeedbacks/Queries/CustomerFeedbackGetQuery.cs
@@ -0,0 +1,15 @@
+﻿using Feedback.Analyzer.Application.Clients.Models;
+using Feedback.Analyzer.Domain.Common.Queries;
+
+namespace Feedback.Analyzer.Application.Clients.Queries;
+
+/// <summary>
+/// Represents a query for retrieving a collection of customer feedback data transfer objects.
+/// </summary>
+public class CustomerFeedbackGetQuery : IQuery<ICollection<CustomerFeedbackDto>>
+{
+    /// <summary>
+    /// Gets the filter to be applied for retrieving customer feedback.
+    /// </summary>
+    public CustomerFeedbackFilter CustomerFeedbackFilter { get; init; } = default!;
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/CustomerFeedbacks/QueryHandlers/CustomerFeedbackGetByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.Clients.Models;
+using Feedback.Analyzer.Application.Clients.Queries;
+using Feedback.Analyzer.Application.Clients.Services;
+using Feedback.Analyzer.Domain.Common.Queries;
+
+namespace Feedback.Analyzer.Infrastructure.CustomerFeedbacks.QueryHandlers;
+
+/// <summary>
+/// Handles the query to retrieve customer feedback by its ID.
+/// </summary>
+public class CustomerFeedbackGetByIdQueryHandler(ICustomerFeedbackService customerFeedbackService, IMapper mapper) 
+    : IQueryHandler<CustomerFeedbackGetByIdQuery, CustomerFeedbackDto?>
+{
+    public async Task<CustomerFeedbackDto?> Handle(CustomerFeedbackGetByIdQuery request,CancellationToken cancellationToken)
+    {
+        var foundCustomerFeedback = await customerFeedbackService.GetByIdAsync
+        (
+            request.ProductId,
+            new QueryOptions()
+            {
+                AsNoTracking = true
+            },
+            cancellationToken
+        );
+
+        return mapper.Map<CustomerFeedbackDto>(foundCustomerFeedback);
+    }
+    
+}
