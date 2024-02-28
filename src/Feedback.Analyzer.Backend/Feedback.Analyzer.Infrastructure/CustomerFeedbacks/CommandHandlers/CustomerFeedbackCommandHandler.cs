3e5c35d0afbd5c215f05240a4c20f32a3b43cc7e
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-02-28 13:33:02 +0500

    Add customer feedback  command

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/CustomerFeedbacks/CommandHandlers/CustomerFeedbackCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.Clients.Commands;
+using Feedback.Analyzer.Application.Clients.Models;
+using Feedback.Analyzer.Application.Clients.Services;
+using Feedback.Analyzer.Domain.Common.Commands;
+using Feedback.Analyzer.Domain.Entities;
+
+namespace Feedback.Analyzer.Infrastructure.CustomerFeedbacks.CommandHandlers;
+
+/// <summary>
+/// Handles the command to update customer feedback.
+/// </summary>
+public class CustomerFeedbackCommandHandler(ICustomerFeedbackService customerFeedbackService,IMapper mapper) 
+    : ICommandHandler<CustomerFeedbackUpdateCommand, CustomerFeedbackDto>
+{
+    public async Task<CustomerFeedbackDto> Handle(CustomerFeedbackUpdateCommand request, CancellationToken cancellationToken)
+    {
+        var customerFeedback = mapper.Map<CustomerFeedback>(request.CustomerFeedback);
+        var updateCustomerFeedback = await customerFeedbackService
+            .UpdateAsync(customerFeedback,new CommandOptions(),cancellationToken);
+
+        return mapper.Map<CustomerFeedbackDto>(updateCustomerFeedback);
+    }
+}
