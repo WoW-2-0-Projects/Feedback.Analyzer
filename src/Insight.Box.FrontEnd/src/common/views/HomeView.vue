ccead3e35a7c3e383bcae58f6391fb20ca41e051
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-02-28 10:44:55 +0500

    small changes :))

--- src/Insight.Box.FrontEnd/src/common/views/HomeView.vue
+++ src/Insight.Box.FrontEnd/src/common/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div class="h-screen w-screen flex items-center justify-center bg-[#333] text-white">
-        <h1>Hi </h1>
+        <h1>Hello Azure!</h1>
     </div>
 
 </template>
