b7dddc0f76fdbb07441182d413890f66e634dbbf
Botirjon <thedavlativich@gmail.com>
2024-02-29 12:07:53 +0500

    Add product queries

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Products/Queries/ProductGetQuery.cs
@@ -0,0 +1,15 @@
+using Feedback.Analyzer.Application.Products.Models;
+using Feedback.Analyzer.Domain.Common.Queries;
+
+namespace Feedback.Analyzer.Application.Products.Queries;
+
+/// <summary>
+/// Represents a query to retrieve a collection of products with optional filtering.
+/// </summary>
+public record ProductGetQuery : IQuery<ICollection<ProductDto>>
+{
+    /// <summary>
+    /// Optional filter criteria for retrieving products.
+    /// </summary>
+    public ProductFilter ProductFilter { get; set; } = default!;
+}
