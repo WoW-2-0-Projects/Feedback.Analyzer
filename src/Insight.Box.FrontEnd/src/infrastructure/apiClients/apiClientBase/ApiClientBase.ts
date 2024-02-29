c42cdea85f736f87dbd505c06823e4b846181a61
Asadbek Rashidov <rashidovasabek264@gmail.com>
2024-02-29 09:29:33 +0500

    Add CRUD methods for Api client base

--- src/Insight.Box.FrontEnd/src/infrastructure/apiClients/apiClientBase/ApiClientBase.ts
+++ src/Insight.Box.FrontEnd/src/infrastructure/apiClients/apiClientBase/ApiClientBase.ts
@@ -41,4 +41,58 @@ export default class ApiClientBase {
             }
         );
     }
+
+    /**
+     * Performs an asynchronous GET request using the internal Axios client, extracting
+     * the API response data.
+     *
+     * @param {string} url - The URL endpoint to send the GET request to.
+     * @param {AxiosRequestConfig} [config] - Optional configuration for the Axios request.
+     * @returns {Promise<T>} A Promise that resolves to the extracted data from the
+     *                       ApiResponse<T> object.
+     */
+    public async getAsync<T>(url: string, config?: AxiosRequestConfig): Promise<ApiResponse<T>> {
+        return (await this.client.get<ApiResponse<T>>(url, config)).data;
+    }
+
+    /**
+     * Performs an asynchronous POST request using the internal Axios client,
+     * extracting the API response data.
+     *
+     * @param {string} url - The URL endpoint to send the POST request to.
+     * @param {any} [data] - Optional data to be included in the POST request body.
+     * @param {AxiosRequestConfig} [config] - Optional configuration for the Axios request.
+     * @returns {Promise<T>} A Promise that resolves to the extracted data from the
+     *                       ApiResponse<T> object.
+     */
+    public async postAsync<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<ApiResponse<T>> {
+        return (await this.client.post<ApiResponse<T>>(url, data, config)).data;
+    }
+
+    /**
+     * Performs an asynchronous PUT request using the internal Axios client,
+     * extracting the API response data.
+     *
+     * @param {string} url - The URL endpoint to send the PUT request to.
+     * @param {any} [data] - Optional data to be included in the PUT request body.
+     * @param {AxiosRequestConfig} [config] - Optional configuration for the Axios request.
+     * @returns {Promise<T>} A Promise that resolves to the extracted data from the
+     *                       ApiResponse<T> object.
+     */
+    public async putAsync<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<ApiResponse<T>> {
+        return (await this.client.put<ApiResponse<T>>(url, data, config)).data;
+    }
+
+    /**
+     * Performs an asynchronous DELETE request using the internal Axios client,
+     * extracting the API response data.
+     *
+     * @param {string} url - The URL endpoint to send the DELETE request to.
+     * @param {AxiosRequestConfig} [config] - Optional configuration for the Axios request.
+     * @returns {Promise<T>} A Promise that resolves to the extracted data from the
+     *                       ApiResponse<T> object.
+     */
+    public async deleteAsync<T>(url: string, config?: AxiosRequestConfig): Promise<ApiResponse<T>> {
+        return (await this.client.delete<ApiResponse<T>>(url, config)).data;
+    }
 }
