265d1f42e496df1cffcf17a65d60a41605f5b6a3
Botirjon <thedavlativich@gmail.com>
2024-02-29 12:12:33 +0500

    Add product validator and product service

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/Products/Validators/ProductValidator.cs
@@ -0,0 +1,54 @@
+using Feedback.Analyzer.Application.Common.Settings;
+using Feedback.Analyzer.Domain.Entities;
+using Feedback.Analyzer.Domain.Enums;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace Feedback.Analyzer.Infrastructure.Products.Validators;
+
+/// <summary>
+/// Validates the properties of a <see cref="Product"/> object.
+/// </summary>
+public class ProductValidator : AbstractValidator<Product>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProductValidator"/> class.
+    /// </summary>
+    /// <param name="validationSettings">The validation settings.</param>
+    public ProductValidator(IOptions<ValidationSettings> validationSettings)
+    {
+        var validationSettingsValue = validationSettings.Value;
+
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(product => product.Name)
+                    .NotEmpty()
+                    .MinimumLength(3)
+                    .MaximumLength(128)
+                    .Matches(validationSettingsValue.NameRegexPattern);
+                RuleFor(product => product.Description)
+                    .NotEmpty()
+                    .MinimumLength(3)
+                    .MaximumLength(512);
+            }
+        );
+        
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(product => product.Name)
+                    .NotEmpty()
+                    .MinimumLength(3)
+                    .MaximumLength(128)
+                    .Matches(validationSettingsValue.NameRegexPattern);
+                RuleFor(product => product.Description)
+                    .NotEmpty()
+                    .MinimumLength(3)
+                    .MaximumLength(512);
+            }
+        );
+    }
+}
