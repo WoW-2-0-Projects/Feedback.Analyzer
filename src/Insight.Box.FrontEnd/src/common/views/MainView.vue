8aebf8afab73aed91d71fc0461c53592c432f771
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-01 10:58:25 +0500

    Create organization tab routes

---
+++ src/Insight.Box.FrontEnd/src/common/views/MainView.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <!-- View content -->
+    <div class="view theme-view-bg content-padding">
+
+        <!-- Site header -->
+        <main-header/>
+
+        <router-view/>
+    </div>
+
+</template>
+<script setup lang="ts">
+import MainHeader from "@/common/components/mainHeader/MainHeader.vue";
+</script>
