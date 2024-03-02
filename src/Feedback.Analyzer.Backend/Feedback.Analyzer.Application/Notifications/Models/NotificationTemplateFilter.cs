07578f06416a93ad3728c3567b3e88c0c6e39dc9
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-03-02 22:52:21 +0500

    Created dto and filter models

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Notifications/Models/NotificationTemplateFilter.cs
@@ -0,0 +1,21 @@
+﻿using Feedback.Analyzer.Domain.Common.Query;
+using Feedback.Analyzer.Domain.Enums;
+
+namespace Feedback.Analyzer.Application.Notifications.Models;
+
+/// <summary>
+/// Represents a filter for querying notification templates with pagination support.
+/// </summary>
+public class NotificationTemplateFilter : FilterPagination
+{
+    public NotificationTemplateFilter()
+    {
+        PageSize = int.MaxValue;
+        PageToken = 1;
+    }
+
+    /// <summary>
+    /// Gets or sets collectio of notification types for filtering templates
+    /// </summary>
+    public IList<NotificationType> TemplateType { get; set; }
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Notifications/Models/SmsTemplateFilter.cs
@@ -0,0 +1,39 @@
+﻿using Feedback.Analyzer.Application.Clients.Models;
+using Feedback.Analyzer.Domain.Common.Query;
+
+namespace Feedback.Analyzer.Application.Notifications.Models;
+
+// <summary>
+/// Represents a filter for querying sms templates with pagination support.
+/// </summary>
+public class SmsTemplateFilter : FilterPagination
+{
+    public SmsTemplateFilter()
+    {
+        PageSize = int.MaxValue;
+        PageToken = 1;
+    }
+
+    /// <summary>
+    /// Overrides base GetHashCode method
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageSize);
+        hashCode.Add(PageToken);
+
+        return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Overrides base Equals method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj) =>
+        obj is ClientFilter clientFilter
+        && clientFilter.GetHashCode() == GetHashCode();
+}
