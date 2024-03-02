44cbfb4507f09cab0b23a739ecd84bab61e406fb
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-02 13:40:11 +0500

    Fix modal closing for content action and add title

--- src/Insight.Box.FrontEnd/src/common/components/modalBase/ModalBase.vue
+++ src/Insight.Box.FrontEnd/src/common/components/modalBase/ModalBase.vue
@@ -3,20 +3,25 @@
     <Teleport to="body">
 
         <!-- Modal background -->
-        <div class="" :class="isActive ? 'modal-bg-overlay-blur' : ''">
+        <div :class="isActive ? 'modal-bg-overlay-blur' : ''" @click="emit('closeModal')">
 
             <!-- Modal container -->
             <div :class="isActive ? ' absolute-y-center' : ' opacity-0'"
                  class="fixed h-fit transform top-1/2 absolute-x-center theme-modal-transition
                      theme-modal-bg modal-border-round theme-modal-border inset-0 z-30 overflow-auto no-scrollbar">
 
-                <div class="w-full h-full relative pt-20">
+                <div class="w-full h-full relative pt-20" @click.stop>
 
                     <!-- Modal close button -->
                     <close-button class="absolute top-5 right-5" @click="emit('closeModal')"/>
 
+                    <!-- Modal header -->
+                    <div class="absolute top-6 absolute-x-center">
+                        <slot name="header"/>
+                    </div>
+
                     <!-- Modal content -->
-                    <slot/>
+                    <slot name="content"/>
 
                 </div>
             </div>
