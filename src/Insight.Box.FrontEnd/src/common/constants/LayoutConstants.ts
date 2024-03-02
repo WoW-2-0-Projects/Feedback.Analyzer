ebbd5b44ea807ee7024ad9a18359eaecad305a92
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-02 15:40:01 +0500

    Add product modal and update product search bar

--- src/Insight.Box.FrontEnd/src/common/constants/LayoutConstants.ts
+++ src/Insight.Box.FrontEnd/src/common/constants/LayoutConstants.ts
@@ -5,4 +5,19 @@
 export class LayoutConstants {
     public static readonly Organizations = 'Organizations';
     public static readonly Products = 'Products';
+
+
+    /* Products */
+
+    public static readonly CreateProduct = 'Create a product';
+    public static readonly EditProduct = 'Edit product ';
+    public static readonly ProductName = 'Product name';
+    public static readonly ProductDescription = 'Product description';
+    public static readonly EnterProductName = 'Enter product name';
+    public static readonly EnterProductDescription = 'Enter product description';
+
+
+    /* Forms */
+    public static readonly Submit = 'Submit';
+    public static readonly Cancel = 'Cancel';
 }
---
+++ src/Insight.Box.FrontEnd/src/modules/products/components/ProductModal.vue
@@ -0,0 +1,98 @@
+<template>
+
+    <ModalBase :isActive="props.isActive" @closeModal="onCloseModal">
+
+        <template v-slot:header>
+            <h1 class="text-primaryContentColor text-xl">
+                {{
+                    isCreate
+                        ? LayoutConstants.CreateProduct :
+                        LayoutConstants.EditProduct
+                }}
+            </h1>
+        </template>
+
+        <!-- Product modal content -->
+        <template v-slot:content>
+
+            <div class="modal-content-padding modal-content-layout">
+
+                <form class="flex flex-col gap-10" @submit.prevent="onSubmit">
+
+                    <!-- Modal inputs -->
+                    <form-input v-model="currentValue.name" :label="LayoutConstants.ProductName"
+                                :placeholder="LayoutConstants.EnterProductName"/>
+
+                    <form-input v-model="currentValue.description" :label="LayoutConstants.ProductDescription"
+                                :placeholder="LayoutConstants.EnterProductDescription"/>
+
+                    <!-- Modal actions -->
+                    <div class="flex gap-10">
+                        <app-button :type="ButtonType.Secondary" class="w-full" :text="LayoutConstants.Cancel"
+                                    @click="onCloseModal"/>
+                        <app-button :type="ButtonType.Primary" class="w-full" :text="LayoutConstants.Submit"
+                                    :role="ButtonRole.Submit" @click="onSubmit"/>
+                    </div>
+
+                </form>
+            </div>
+
+        </template>
+
+    </ModalBase>
+
+</template>
+
+<script setup lang="ts">
+
+import {defineEmits, type PropType, ref, watch} from 'vue';
+import {LayoutConstants} from "@/common/constants/LayoutConstants";
+import ModalBase from "@/common/components/modalBase/ModalBase.vue";
+import FormInput from "@/common/components/formInput/FormInput.vue";
+import AppButton from "@/common/components/appButton/AppButton.vue";
+import {ButtonType} from "@/common/components/appButton/ButtonType";
+import {Product} from "@/modules/products/models/Product";
+import {ButtonRole} from "@/common/components/appButton/ButtonRole";
+
+const props = defineProps({
+    product: {
+        type: Object as PropType<Product>,
+        default: new Product()
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+    isCreate: {
+        type: Boolean,
+        default: true
+    }
+});
+
+const emit = defineEmits<{
+    (e: 'closeModal'): void
+    (e: 'submit', product: Product): void
+}>();
+
+const currentValue = ref<Product>(props.product);
+
+const onCloseModal = () => {
+    // Reset values
+    if(!props.isCreate)
+        props.product = new Product();
+
+    emit('closeModal');
+}
+
+const onSubmit = async () => {
+    console.log('test');
+
+    emit('submit', props.product);
+    emit('closeModal');
+}
+
+watch(() => props.product, (newValue) => {
+    currentValue.value = newValue;
+});
+
+</script>
