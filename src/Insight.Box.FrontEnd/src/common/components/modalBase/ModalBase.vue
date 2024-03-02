0a50a28a3bb01a6d4863a9e903e47914896ab24c
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-02 13:08:19 +0500

    Add modal base component

---
+++ src/Insight.Box.FrontEnd/src/common/components/modalBase/ModalBase.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <Teleport to="body">
+
+        <!-- Modal background -->
+        <div class="" :class="isActive ? 'modal-bg-overlay-blur' : ''">
+
+            <!-- Modal container -->
+            <div :class="isActive ? ' absolute-y-center' : ' opacity-0'"
+                 class="fixed h-fit transform top-1/2 absolute-x-center theme-modal-transition
+                     theme-modal-bg modal-border-round theme-modal-border inset-0 z-30 overflow-auto no-scrollbar">
+
+                <div class="w-full h-full relative pt-20">
+
+                    <!-- Modal close button -->
+                    <close-button class="absolute top-5 right-5" @click="emit('closeModal')"/>
+
+                    <!-- Modal content -->
+                    <slot/>
+
+                </div>
+            </div>
+
+        </div>
+
+    </Teleport>
+
+</template>
+
+<script setup lang="ts">
+
+import CloseButton from "@/common/components/buttons/CloseButton.vue";
+
+const props = defineProps({
+    isActive: {
+        type: Boolean,
+        default: false
+    }
+});
+
+
+const emit = defineEmits(['closeModal']);
+
+</script>
