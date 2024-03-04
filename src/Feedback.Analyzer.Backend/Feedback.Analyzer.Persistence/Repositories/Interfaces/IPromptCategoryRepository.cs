5d7ffd1cfe66546cf161fd8a46e8b2df0cf6eae1
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-04 20:49:57 +0500

    Add Analysis Prompt Category Repository

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Repositories/Interfaces/IPromptCategoryRepository.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Repositories/Interfaces/IPromptCategoryRepository.cs
@@ -4,9 +4,16 @@ using Feedback.Analyzer.Domain.Entities;
 
 namespace Feedback.Analyzer.Persistence.Repositories.Interfaces;
 
+/// <summary>
+/// Interface for managing prompt category entities in the repository.
+/// </summary>
 public interface IPromptCategoryRepository
 {
-    IQueryable<AnalysisPromptCategory> Get(
-        Expression<Func<AnalysisPromptCategory, bool>>? predicate = default, 
-        QueryOptions queryOptions = default);
+    /// <summary>
+    /// Retrieves a queryable collection of prompt category entities based on the specified predicate.
+    /// </summary>
+    /// <param name="predicate">A predicate to filter the prompt categories (optional).</param>
+    /// <param name="queryOptions">Query options for the retrieval.</param>
+    /// <returns>A queryable collection of prompt category entities.</returns>
+    IQueryable<AnalysisPromptCategory> Get(Expression<Func<AnalysisPromptCategory, bool>>? predicate = default, QueryOptions queryOptions = default);
 }
