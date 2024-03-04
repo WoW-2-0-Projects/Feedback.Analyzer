ff3dd31d3e13240ffcc23750d91701b6c7e443b4
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-04 15:46:20 +0500

    Add Analysis Prompt Configuration

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Migrations/20240304102357_Add Analysis Prompt Entity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Feedback.Analyzer.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnalysisPromptEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Clients",
+                newName: "EmailAddress");
+
+            migrationBuilder.CreateTable(
+                name: "Prompts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Prompt = table.Column<string>(type: "character varying(32768)", maxLength: 32768, nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false),
+                    Revision = table.Column<int>(type: "integer", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeletedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prompts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prompts");
+
+            migrationBuilder.RenameColumn(
+                name: "EmailAddress",
+                table: "Clients",
+                newName: "Email");
+        }
+    }
+}
