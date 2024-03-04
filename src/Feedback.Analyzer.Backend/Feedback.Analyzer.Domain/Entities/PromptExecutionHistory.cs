b957899901b0d25d3be5e4b016dfaa4a645e1cfb
Bunyod <kbunyod3011@gmail.com>
2024-03-04 12:20:28 +0500

    Add PromptExecutionHistory entity

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Domain/Entities/PromptExecutionHistory.cs
@@ -0,0 +1,18 @@
+﻿using Feedback.Analyzer.Domain.Common.Entities;
+using Feedback.Analyzer.Domain.Common.Exeptions;
+
+namespace Feedback.Analyzer.Domain.Entities;
+
+public class PromptExecutionHistory : Entity
+{ 
+    public Guid PromptId { get; set; }
+
+    public string? Result { get; set; }
+
+    public string? Exception { get; set; } 
+    
+    public TimeSpan ExecutionTime { get; set; } 
+    
+    public TimeSpan ExecutionDuration { get; set; }
+    
+}
