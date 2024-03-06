aff7f0327e1a936205ef92ab552bd9307c85e030
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-06 12:47:36 +0500

    Add repository

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Repositories/FeedbackWorkflowRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using Feedback.Analyzer.Domain.Common.Commands;
+using Feedback.Analyzer.Domain.Common.Queries;
+using Feedback.Analyzer.Domain.Entities;
+using Feedback.Analyzer.Persistence.DataContexts;
+using Feedback.Analyzer.Persistence.Repositories.Interfaces;
+
+namespace Feedback.Analyzer.Persistence.Repositories;
+
+/// <summary>
+/// Represents a repository for managing feedback workflows.
+/// </summary>
+public class FeedbackWorkflowRepository(AppDbContext appDbContext) 
+    : EntityRepositoryBase
+        <FeedbackWorkflow, AppDbContext>(
+            appDbContext
+            ), IFeedbackWorkflowRepository
+{
+    public new IQueryable<FeedbackWorkflow> Get(Expression<Func<FeedbackWorkflow, bool>>? predicate = default, QueryOptions queryOptions = default)
+        => base.Get(predicate, queryOptions);
+
+    public new ValueTask<FeedbackWorkflow?> GetByIdAsync(Guid workflowId, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default)
+        => base.GetByIdAsync(workflowId, queryOptions, cancellationToken);
+
+    public new ValueTask<FeedbackWorkflow> CreateAsync(FeedbackWorkflow feedbackWorkflow, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default)
+        => base.CreateAsync(feedbackWorkflow, commandOptions, cancellationToken);
+
+    public new ValueTask<FeedbackWorkflow> UpdateAsync(FeedbackWorkflow feedbackWorkflow,
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default)
+        => base.UpdateAsync(feedbackWorkflow, commandOptions, cancellationToken);
+
+    public new ValueTask<FeedbackWorkflow> DeleteAsync(FeedbackWorkflow feedbackWorkflow,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+        => base.DeleteAsync(feedbackWorkflow, commandOptions, cancellationToken);
+
+    public new ValueTask<FeedbackWorkflow> DeleteByIdAsync(Guid workflowId,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+        => base.DeleteByIdAsync(workflowId, commandOptions, cancellationToken);
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Repositories/Interfaces/IFeedbackWorkflowRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq.Expressions;
+using Feedback.Analyzer.Domain.Common.Commands;
+using Feedback.Analyzer.Domain.Common.Queries;
+using Feedback.Analyzer.Domain.Entities;
+
+namespace Feedback.Analyzer.Persistence.Repositories.Interfaces;
+
+/// <summary>
+/// Represents a repository interface for managing FeedbackWorkflow entities.
+/// </summary>
+public interface IFeedbackWorkflowRepository
+{
+    /// <summary>
+    /// Retrieves FeedbackWorkflow entities based on the specified predicate and query options.
+    /// </summary>
+    /// <param name="predicate">Optional predicate to filter the entities.</param>
+    /// <param name="queryOptions">Options for customizing the query behavior.</param>
+    /// <returns>An IQueryable of FeedbackWorkflow entities.</returns>
+    public IQueryable<FeedbackWorkflow> Get(Expression<Func<FeedbackWorkflow, bool>>? predicate = default, QueryOptions queryOptions = default);
+
+    /// <summary>
+    /// Retrieves an FeedbackWorkflow entity asynchronously by its unique identifier.
+    /// </summary>
+    /// <param name="workflowId">The unique identifier of the FeedbackWorkflow entity.</param>
+    /// <param name="queryOptions">Options for customizing the query behavior.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the retrieved FeedbackWorkflow entity.</returns>
+    public ValueTask<FeedbackWorkflow> GetByIdAsync(Guid workflowId, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously creates a new FeedbackWorkflow entity.
+    /// </summary>
+    /// <param name="feedbackWorkflow">The FeedbackWorkflow entity to be created.</param>
+    /// <param name="commandOptions">Options for customizing the command behavior.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the newly created FeedbackWorkflow entity.</returns>
+    public ValueTask<FeedbackWorkflow> CreateAsync(FeedbackWorkflow feedbackWorkflow, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously updates an existing FeedbackWorkflow entity.
+    /// </summary>
+    /// <param name="feedbackWorkflow">The FeedbackWorkflow entity to be updated.</param>
+    /// <param name="commandOptions">Options for customizing the command behavior.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the updated FeedbackWorkflow entity.</returns>
+    public ValueTask<FeedbackWorkflow> UpdateAsync(FeedbackWorkflow feedbackWorkflow, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously deletes the specified FeedbackWorkflow entity.
+    /// </summary>
+    /// <param name="feedbackWorkflow">The FeedbackWorkflow entity to be deleted.</param>
+    /// <param name="commandOptions">Options for customizing the command behavior.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the deleted FeedbackWorkflow entity.</returns>
+    public ValueTask<FeedbackWorkflow> DeleteAsync(FeedbackWorkflow feedbackWorkflow, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously deletes the FeedbackWorkflow entity with the specified identifier.
+    /// </summary>
+    /// <param name="workflowId">The unique identifier of the FeedbackWorkflows entity to be deleted.</param>
+    /// <param name="commandOptions">Options for customizing the command behavior.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the deleted FeedbackWorkflows entity.</returns>
+    public ValueTask<FeedbackWorkflow> DeleteByIdAsync(Guid workflowId, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+}
