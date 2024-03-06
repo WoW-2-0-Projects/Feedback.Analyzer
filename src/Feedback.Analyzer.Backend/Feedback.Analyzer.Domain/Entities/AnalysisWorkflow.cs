9dd60b884f66d896c59c2e21b3116ff564b20c0d
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-06 17:07:19 +0500

    Add Entities

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Domain/Entities/AnalysisWorkflow.cs
@@ -0,0 +1,20 @@
+using Feedback.Analyzer.Domain.Common.Entities;
+using Feedback.Analyzer.Domain.Enums;
+
+namespace Feedback.Analyzer.Domain.Entities;
+
+/// <summary>
+/// Represents an analysis workflow entity.
+/// </summary>
+public class AnalysisWorkflow : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the analysis workflow.
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the type of the analysis workflow.
+    /// </summary>
+    public WorkflowType Type { get; set; } 
+}
