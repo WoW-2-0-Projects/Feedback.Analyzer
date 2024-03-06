ae67350576c3038e940a0eba7d1a2d22b05abcd2
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-06 17:08:42 +0500

    some changes

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/Prompts/Models/AnalysisPromptDto.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/Prompts/Models/AnalysisPromptDto.cs
@@ -7,43 +7,19 @@ namespace Feedback.Analyzer.Application.Common.Prompts.Models;
 /// </summary>
 public class AnalysisPromptDto
 {
-    /// <summary>
-    /// Gets or sets the ID of the analysis prompt.
-    /// </summary>
     public Guid Id { get; set; }
     
-    /// <summary>
-    /// Gets or sets the type of feedback analysis prompt.
-    /// </summary>
-    // public FeedbackAnalysisPromptType Type { get; set; }
+    public FeedbackAnalysisPromptCategory Category { get; set; }
     
-    /// <summary>
-    /// Gets or sets the text of the prompt.
-    /// </summary>
     public string Prompt { get; set; } = default!;
     
-    /// <summary>
-    /// Gets or sets the version of the analysis prompt.
-    /// </summary>
     public int Version { get; set; }
     
-    /// <summary>
-    /// Gets or sets the revision of the analysis prompt.
-    /// </summary>
     public int Revision { get; set; }
     
-    /// <summary>
-    /// Gets or sets the category ID of the analysis prompt.
-    /// </summary>
     public Guid CategoryId { get; set; }
     
-    /// <summary>
-    /// Gets or sets the creation time of the analysis prompt.
-    /// </summary>
     public DateTimeOffset CreatedTime { get; set; }
     
-    /// <summary>
-    /// Gets or sets the modified time of the analysis prompt.
-    /// </summary>
     public DateTimeOffset ModifiedTime { get; set; }
 }
