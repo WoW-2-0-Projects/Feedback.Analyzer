44f18dd5f6636796188fe7d8278c061104a42f34
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-06 17:03:24 +0500

    Add prompt  category queries and query handlers

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/Common/Prompts/QueryHandlers/PromptResultGetByCategoryIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.Common.Prompts.Models;
+using Feedback.Analyzer.Application.Common.Prompts.Queries;
+using Feedback.Analyzer.Application.Common.Prompts.Services;
+using Feedback.Analyzer.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedback.Analyzer.Infrastructure.Common.Prompts.QueryHandlers;
+
+public class PromptResultGetByCategoryIdQueryHandler(IMapper mapper, IPromptService promptService) 
+    : IQueryHandler<PromptResultGetByCategoryIdQuery, ICollection<PromptResultDto>>
+{
+    public async Task<ICollection<PromptResultDto>> Handle(PromptResultGetByCategoryIdQuery request, CancellationToken cancellationToken)
+    {
+        var matchedPrompts = await promptService.Get
+            (
+                prompt => prompt.CategoryId == request.CategoryId,
+                queryOptions: new QueryOptions()
+                {
+                    AsNoTracking = true
+                }
+                ).ToListAsync(cancellationToken: cancellationToken);
+        
+        return mapper.Map<ICollection<PromptResultDto>>(matchedPrompts);
+    }
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/Common/PromptsCategories/QueryHandlers/PromptCategoryGetQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Feedback.Analyzer.Application.Common.PromptCategory.Models;
+using Feedback.Analyzer.Application.Common.PromptCategory.Quearies;
+using Feedback.Analyzer.Application.Common.PromptCategory.Services;
+using Feedback.Analyzer.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedback.Analyzer.Infrastructure.Common.PromptsCategories.QueryHandlers;
+
+public class PromptCategoryGetQueryHandler(IMapper mapper, IPromptCategoryService promptCategoryService)
+    : IQueryHandler<PromptCategoryGetQuery, ICollection<AnalysisPromptCategoryDto>>
+{
+    public async Task<ICollection<AnalysisPromptCategoryDto>> Handle(PromptCategoryGetQuery request, CancellationToken cancellationToken)
+    {
+        var matchedPromptCategory = await promptCategoryService.Get(
+            request.Filter,
+            new QueryOptions
+            {
+                AsNoTracking = true
+            }
+        )
+        .Include(promptCategory => promptCategory.Prompts)
+        //.ProjectTo<AnalysisPromptCategoryDto>(mapper.ConfigurationProvider)
+        .ToListAsync(cancellationToken);
+
+        var result = mapper.Map<ICollection<AnalysisPromptCategoryDto>>(matchedPromptCategory);
+        
+        return result;
+    }
+}
