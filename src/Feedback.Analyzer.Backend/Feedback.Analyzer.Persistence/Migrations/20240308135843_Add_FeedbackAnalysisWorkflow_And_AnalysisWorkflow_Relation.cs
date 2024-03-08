f6cd58c79d04f4cf4de985deb1661dc8f150ae91
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-08 19:02:06 +0500

    Add feedback analysis workflow and analysis workflow relation

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Migrations/20240308135843_Add_FeedbackAnalysisWorkflow_And_AnalysisWorkflow_Relation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Feedback.Analyzer.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_FeedbackAnalysisWorkflow_And_AnalysisWorkflow_Relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FeedbackAnalysisWorkflows",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeedbackAnalysisWorkflows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FeedbackAnalysisWorkflows_AnalysisWorkflows_Id",
+                        column: x => x.Id,
+                        principalTable: "AnalysisWorkflows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FeedbackAnalysisWorkflows");
+        }
+    }
+}
