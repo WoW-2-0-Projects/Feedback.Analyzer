3cd320c0386af77aecbb0ef853b4c21233e95164
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-12 15:03:27 +0500

    feature Dto and filter

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/FeedbackAnalysisWorkflows/Models/FeedbackAnalysisWorkflowDto.cs
@@ -0,0 +1,26 @@
+﻿using Feedback.Analyzer.Domain.Common.Entities;
+using Feedback.Analyzer.Domain.Enums;
+
+namespace Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Models;
+
+/// <summary>
+/// Represents a data transfer object (DTO) for feedback analysis workflow
+/// </summary>
+public class FeedbackAnalysisWorkflowDto : Entity
+{
+    /// <summary>
+    /// Gets or sets the Id of product associated with the feedback analysis workflow
+    /// </summary>
+    public Guid ProductId { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the name of the feedback analysis workflow.
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the type of the feedback analysis workflow.
+    /// </summary>
+    public WorkflowType Type { get; set; }
+
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/FeedbackAnalysisWorkflows/Models/FeedbackAnalysisWorkflowFilter.cs
@@ -0,0 +1,29 @@
+﻿using Feedback.Analyzer.Domain.Common.Query;
+
+namespace Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Models;
+
+public class FeedbackAnalysisWorkflowFilter : FilterPagination
+{
+    /// <summary>
+    /// Override base GetHashCode method
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        
+            hashCode.Add(PageSize);
+            hashCode.Add(PageToken);
+
+            return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Override base Equals method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj) =>
+        obj is FeedbackAnalysisWorkflowFilter feedbackAnalysisWorkflowFilter
+        && feedbackAnalysisWorkflowFilter.GetHashCode() == GetHashCode();
+}
