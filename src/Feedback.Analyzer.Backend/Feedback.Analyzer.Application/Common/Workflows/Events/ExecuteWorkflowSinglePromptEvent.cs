09fd3646a0f0b16452aeef03cfd69c0358a41b1b
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-12 14:03:17 +0500

    Add comment ExecuteWorkflowSinglePromptEvent

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/Workflows/Events/ExecuteWorkflowSinglePromptEvent.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/Workflows/Events/ExecuteWorkflowSinglePromptEvent.cs
@@ -2,6 +2,9 @@ using Feedback.Analyzer.Domain.Common.Events;
 
 namespace Feedback.Analyzer.Application.Common.Workflows.Events;
 
+/// <summary>
+/// Represents an event for executing a workflow with a single prompt.
+/// </summary>
 public class ExecuteWorkflowSinglePromptEvent : Event
 {
     public Guid WorkflowId { get; init; }
