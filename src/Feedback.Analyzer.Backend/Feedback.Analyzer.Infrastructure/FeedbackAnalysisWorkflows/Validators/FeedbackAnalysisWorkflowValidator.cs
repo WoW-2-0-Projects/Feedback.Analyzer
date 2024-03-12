c4e98e80d11fcfdfec55a8428fa8b43822af4a91
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-12 15:02:29 +0500

    fix and feature validators

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisWorkflows/Validators/FeedbackAnalysisWorkflowValidator.cs
@@ -0,0 +1,29 @@
+﻿using Feedback.Analyzer.Application.Common.AnalysisWorkflows.Services;
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Services;
+using Feedback.Analyzer.Domain.Entities;
+using Feedback.Analyzer.Domain.Enums;
+using FluentValidation;
+
+namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisWorkflows.Validators;
+
+public class FeedbackAnalysisWorkflowValidator
+    : AbstractValidator<FeedbackAnalysisWorkflow>
+{
+    public FeedbackAnalysisWorkflowValidator(IAnalysisWorkflowService analysisWorkflowService)
+    {
+        RuleSet(EntityEvent.OnCreate.ToString(),
+             () =>
+            {
+                RuleFor(feedbackAnalysisWorkflow => feedbackAnalysisWorkflow.Id)
+                    .NotEmpty()
+                    .CustomAsync(async (id, validationContext, cancellationToken) =>
+                    {
+                        var exists = (await analysisWorkflowService.CheckByIdAsync(id, cancellationToken)) != null;
+
+                        if (!exists)
+                            validationContext.AddFailure(
+                                "Analysis workflow doesn't exist for this feedback analysis workflow");
+                    });
+            });
+    }
+}
