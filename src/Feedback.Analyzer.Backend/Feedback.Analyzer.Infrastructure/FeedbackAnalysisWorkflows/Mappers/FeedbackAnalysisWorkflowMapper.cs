21f2ed6e9be765e473e601bd45a8418da2eb8da3
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-12 15:03:01 +0500

    feature Mappers

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisWorkflows/Mappers/FeedbackAnalysisWorkflowMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Models;
+using Feedback.Analyzer.Domain.Entities;
+
+namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisWorkflows.Mappers;
+
+public class FeedbackAnalysisWorkflowMapper : Profile
+{
+    public FeedbackAnalysisWorkflowMapper()
+    {
+        CreateMap<FeedbackAnalysisWorkflow, FeedbackAnalysisWorkflowDto>().ReverseMap();
+    }
+}
