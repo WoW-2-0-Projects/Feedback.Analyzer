db1749ae4c8d1a028b5597a49e9f2c21ce26d83d
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-12 17:58:03 +0500

    Add workflow execution option repository and efcore extensions

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Extensions/EfCoreExtensions.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Extensions/EfCoreExtensions.cs
@@ -1,6 +1,31 @@
-﻿namespace Feedback.Analyzer.Persistence.Extensions;
+﻿using System.Collections.Immutable;
+using Feedback.Analyzer.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
-public class EfCoreExtensions
+namespace Feedback.Analyzer.Persistence.Extensions;
+
+public static class EfCoreExtensions
 {
-    
+    public static SetPropertyCalls<T> MapToSetPropertyCalls<T>(
+        this SetPropertyCalls<T> setPropertyCalls,
+        IImmutableList<(Func<T, object> propertySelector, Func<T, object> valueSelector)> setPropertyExecutors
+    )
+    {
+        foreach (var propertyExecutor in setPropertyExecutors)
+            setPropertyCalls.SetProperty(propertyExecutor.propertySelector, propertyExecutor.valueSelector);
+
+        return setPropertyCalls;
+    }
+
+    public static IQueryable<TSource> ApplyTrackingMode<TSource>(this IQueryable<TSource> source, QueryTrackingMode trackingMode) where TSource : class
+    {
+        return trackingMode switch
+        {
+            QueryTrackingMode.AsTracking => source,
+            QueryTrackingMode.AsNoTracking => source.AsNoTracking(),
+            QueryTrackingMode.AsNoTrackingWithIdentityResolution => source.AsNoTrackingWithIdentityResolution(),
+            _ => source
+        };
+    }
 }
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Repositories/WorkflowExecutionOptionRepository.cs
@@ -0,0 +1,70 @@
+﻿using Feedback.Analyzer.Domain.Common.Queries;
+using Feedback.Analyzer.Domain.Entities;
+using Feedback.Analyzer.Persistence.Caching.Brokers;
+using Feedback.Analyzer.Persistence.DataContexts;
+using Feedback.Analyzer.Persistence.Extensions;
+using Feedback.Analyzer.Persistence.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedback.Analyzer.Persistence.Repositories;
+
+public class WorkflowExecutionOptionRepository(AppDbContext dbContext, ICacheBroker cacheBroker)
+    : EntityRepositoryBase<WorkflowExecutionOption, AppDbContext>(dbContext, cacheBroker),
+      IWorkflowExecutionOptionRepository
+{
+    public async ValueTask<WorkflowExecutionOption?> GetByIdAndIncludeAllGrandChildrenAsync(
+        Guid optionId, QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        var executionOption = await Get(executionOption => executionOption.Id == optionId, queryOptions)
+                                    .Include(executionOption => executionOption.Workflow)
+                                    .Include(executionOption => executionOption.AnalysisPromptCategory)
+                                    .ThenInclude(category => category.SelectedPrompt)
+                                    .Include(executionOption => executionOption.ChildExecutionOptions)
+                                    .AsSplitQuery()
+                                    .ApplyTrackingMode(queryOptions.TrackingMode)
+                                    .FirstOrDefaultAsync(cancellationToken);
+
+        if (executionOption is not null)
+        {
+            executionOption.AnalysisPromptCategory.SelectedPrompt!.Category = executionOption.AnalysisPromptCategory;
+            executionOption.ChildExecutionOptions = await LoadAllChildrenAsync(executionOption.Id, queryOptions, cancellationToken);
+        }
+        
+        return executionOption;
+    }
+    /// <summary>
+    /// Loads all children of the given execution option
+    /// </summary>
+    /// <param name="parentOptionId"></param>
+    /// <param name="queryOptions"></param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    private async ValueTask<IReadOnlyList<WorkflowExecutionOption>> LoadAllChildrenAsync(
+        Guid parentOptionId,
+        QueryOptions queryOptions,
+        CancellationToken cancellationToken = default
+    )
+    {
+        // Load children
+        var childrenOptions = await Get(executionOption => executionOption.ParentId == parentOptionId, queryOptions)
+                                    .Include(executionOption => executionOption.AnalysisPromptCategory)
+                                    .ThenInclude(category => category.SelectedPrompt)
+                                    .Include(executionOption => executionOption.ChildExecutionOptions)
+                                    .AsSplitQuery()
+                                    .ToListAsync(cancellationToken: cancellationToken);
+        
+        // Load grand children
+        await Task.WhenAll(
+            childrenOptions.Select(
+                async childrenOption =>
+                {
+                    childrenOption.AnalysisPromptCategory.SelectedPrompt!.Category = childrenOption.AnalysisPromptCategory;
+                    childrenOption.ChildExecutionOptions = await LoadAllChildrenAsync(childrenOption.Id, queryOptions, cancellationToken);
+                }
+            )
+        );
+        return childrenOptions;
+    }
+    
+    
+}
