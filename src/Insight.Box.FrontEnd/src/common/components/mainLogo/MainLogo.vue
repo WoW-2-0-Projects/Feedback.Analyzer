fe3c8555d68e5a47348280e1dce9ee1cdccd26c1
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-13 10:58:16 +0500

    Add main logo

---
+++ src/Insight.Box.FrontEnd/src/common/components/mainLogo/MainLogo.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <div class="text-secondaryContentColor flex items-center justify-center basis-1/6">
+        <img src="@/assets/logo.png" alt="test" class="w-20 h-20">
+        <h1 class="text-xl">Insight Box</h1>
+    </div>
+
+</template>
