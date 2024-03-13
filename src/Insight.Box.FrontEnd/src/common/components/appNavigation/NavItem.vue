47bb61fe21b43454baed8d2a3d34ed5c06fdd159
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-13 11:00:19 +0500

    Add navigation item component

---
+++ src/Insight.Box.FrontEnd/src/common/components/appNavigation/NavItem.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div class="py-1 px-4 rounded-full bg-secondaryContentColor text-secondaryContentColor uppercase"
+        :class="isCurrentRouteSelected ? 'bg-opacity-30' : 'bg-opacity-0 text-opacity-50' "
+    >
+        <router-link class="text-base" :to="route.path">{{ route.name }}</router-link>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+
+import {onMounted, type PropType, ref} from "vue";
+import {RouterService} from "@/infrastructure/services/router/RouterService";
+import type {RouteLocationNormalized, RouteRecordRaw} from "vue-router";
+
+const routerService = new RouterService();
+const isCurrentRouteSelected = ref<boolean>();
+
+const props = defineProps({
+    route: {
+        type: Object as PropType<RouteRecordRaw>,
+        required: true
+    }
+});
+
+// const isCurrentRouteActive = computed(() => {
+//
+// });
+
+onMounted(() => {
+    routerService.addRouteChangeListener(handleRouteChange);
+});
+
+const handleRouteChange = (route: RouteLocationNormalized) => {
+    isCurrentRouteSelected.value = route.path === props.route.path;
+    // console.log('route changed', route.path);
+    // console.log('route changed', props.route.path);
+    // console.log('route changed', route.path === props.route.path);
+}
+
+</script>
