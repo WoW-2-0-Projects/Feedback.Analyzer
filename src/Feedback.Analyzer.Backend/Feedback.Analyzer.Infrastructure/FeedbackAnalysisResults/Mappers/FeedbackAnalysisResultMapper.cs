503090a971844ff6d4cd081f777ec68af3514f8f
Botirjon <thedavlativich@gmail.com>
2024-03-14 15:14:09 +0500

    Add feedback analysis result dto and mapper

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisResults/Mappers/FeedbackAnalysisResultMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.FeedbackAnalysisResults.Models;
+using Feedback.Analyzer.Domain.Entities;
+
+namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisResults.Mappers;
+
+/// <summary>
+/// Mapper class for mapping between <see cref="FeedbackAnalysisResult"/> and <see cref="FeedbackAnalysisResultDto"/>.
+/// </summary>
+public class FeedbackAnalysisResultMapper : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeedbackAnalysisResultMapper"/> class.
+    /// </summary>
+    public FeedbackAnalysisResultMapper()
+    {
+        CreateMap<FeedbackAnalysisResult, FeedbackAnalysisResultDto>().ReverseMap();
+    }
+}
