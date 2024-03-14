475cc68e0d0a3037100467c45fcecccdc3899d32
Botirjon <thedavlativich@gmail.com>
2024-03-14 15:13:39 +0500

    Add feedback analysis result queries and query handlers

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisResults/QueryHandlers/FeedbackAnalysisResultGetByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.FeedbackAnalysisResults.Models;
+using Feedback.Analyzer.Application.FeedbackAnalysisResults.Queries;
+using Feedback.Analyzer.Application.FeedbackAnalysisResults.Services;
+using Feedback.Analyzer.Domain.Common.Queries;
+
+namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisResults.QueryHandlers;
+
+/// <summary>
+/// Handles the query to retrieve a feedback analysis result by workflow result ID.
+/// </summary>
+public class FeedbackAnalysisResultGetByIdQueryHandler(IMapper mapper, IFeedbackAnalysisResultService feedbackAnalysisResultService) 
+    : IQueryHandler<FeedbackAnalysisResultGetByWorkflowResultIdQuery, FeedbackAnalysisResultDto?>
+{
+    public async Task<FeedbackAnalysisResultDto?> Handle(FeedbackAnalysisResultGetByWorkflowResultIdQuery request, CancellationToken cancellationToken)
+    {
+        var foundFeedbackAnalysisResult = await feedbackAnalysisResultService.GetByIdAsync(
+            request.WorkflowResultId,
+            new QueryOptions()
+            {
+                AsNoTracking = true
+            },
+            cancellationToken
+        );
+
+        return mapper.Map<FeedbackAnalysisResultDto>(foundFeedbackAnalysisResult);
+    }
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisResults/QueryHandlers/FeedbackAnalysisResultGetQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.FeedbackAnalysisResults.Models;
+using Feedback.Analyzer.Application.FeedbackAnalysisResults.Queries;
+using Feedback.Analyzer.Application.FeedbackAnalysisResults.Services;
+using Feedback.Analyzer.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisResults.QueryHandlers;
+
+/// <summary>
+/// Handles the query to retrieve feedback analysis results.
+/// </summary>
+public class FeedbackAnalysisResultGetQueryHandler(IMapper mapper, IFeedbackAnalysisResultService feedbackAnalysisResultService)
+    : IQueryHandler<FeedbackAnalysisResultGetQuery,ICollection<FeedbackAnalysisResultDto>>
+{
+    public async Task<ICollection<FeedbackAnalysisResultDto>> Handle(FeedbackAnalysisResultGetQuery request, CancellationToken cancellationToken)
+    {
+        var matchedFeedbackAnalysisResult =
+            await feedbackAnalysisResultService.Get(request.Filter, new QueryOptions() { AsNoTracking = true })
+                .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<FeedbackAnalysisResultDto>>(matchedFeedbackAnalysisResult);
+    }
+}
