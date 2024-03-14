f0ea97f0ed3d5322b64b17ccb2cb40d15d3eefdb
Botirjon <thedavlativich@gmail.com>
2024-03-14 15:14:09 +0500

    Add feedback analysis result dto and mapper

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/FeedbackAnalysisResults/Models/FeedbackAnalysisResultDto.cs
@@ -0,0 +1,49 @@
+﻿using Feedback.Analyzer.Domain.Entities;
+
+namespace Feedback.Analyzer.Application.FeedbackAnalysisResults.Models;
+
+/// <summary>
+/// Represents the DTO (Data Transfer Object) for feedback analysis results.
+/// </summary>
+public class FeedbackAnalysisResultDto
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the feedback analysis result.
+    /// </summary>
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the relevance of the feedback.
+    /// </summary>
+    public FeedbackRelevance FeedbackRelevance { get; set;}
+    
+    /// <summary>
+    /// Gets or sets the opinion conveyed by the feedback.
+    /// </summary>
+    public FeedbackOpinion FeedbackOpinion { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the actionable points extracted from the feedback.
+    /// </summary>
+    public FeedbackActionablePoints FeedbackActionablePoints { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the entities mentioned in the feedback.
+    /// </summary>
+    public FeedbackEntities FeedbackEntities { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the metrics associated with the feedback.
+    /// </summary>
+    public FeedbackMetrics FeedbackMetrics { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the unique identifier for the customer feedback associated with this analysis result.
+    /// </summary>
+    public Guid CustomerFeedbackId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the customer feedback associated with this analysis result.
+    /// </summary>
+    public CustomerFeedback CustomerFeedback { get; set; } = default!;
+}
