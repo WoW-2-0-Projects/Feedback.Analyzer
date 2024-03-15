0ff1638c23995ee5f6d681ee48258a7ab8ab2a18
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-15 21:05:51 +0500

    feature:  App and config file

--- src/Insight.Box.FrontEnd/src/App.vue
+++ src/Insight.Box.FrontEnd/src/App.vue
@@ -2,8 +2,16 @@
 
     <main-view/>
 
+    <auth-modal :isActive="isAuthModalActive" @closeModal="isAuthModalActive = false"/>
+
 </template>
 
 <script setup lang="ts">
 import MainView from "@/common/views/MainView.vue";
+import AuthModal from "@/modules/accounts/companents/AuthModal.vue";
+import {ref} from "vue";
+import {AuthenticationService} from "@/modules/accounts/services/AuthenticationService";
+
+const authService = new AuthenticationService();
+const isAuthModalActive = ref<boolean>(!authService.isLoggedIn());
 </script>
