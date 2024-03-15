d512d686edd178b07bb966d241a2300059611369
Botirjon <thedavlativich@gmail.com>
2024-03-15 15:29:10 +0500

    Update feedback analysis result mapper

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisResults/Mappers/FeedbackAnalysisResultMapper.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisResults/Mappers/FeedbackAnalysisResultMapper.cs
@@ -4,14 +4,8 @@ using Feedback.Analyzer.Domain.Entities;
 
 namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisResults.Mappers;
 
-/// <summary>
-/// Mapper class for mapping between <see cref="FeedbackAnalysisResult"/> and <see cref="FeedbackAnalysisResultDto"/>.
-/// </summary>
 public class FeedbackAnalysisResultMapper : Profile
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="FeedbackAnalysisResultMapper"/> class.
-    /// </summary>
     public FeedbackAnalysisResultMapper()
     {
         CreateMap<FeedbackAnalysisResult, FeedbackAnalysisResultDto>().ReverseMap();
