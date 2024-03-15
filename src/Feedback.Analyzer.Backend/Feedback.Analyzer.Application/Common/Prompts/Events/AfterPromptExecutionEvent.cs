97e6f82976d1bf2c58b08ef12620fc53b31c46c3
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-03-15 17:30:53 +0500

    Add AfterPromptExecution Event and Handler

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/Prompts/Events/AfterPromptExecutionEvent.cs
@@ -0,0 +1,22 @@
+using Feedback.Analyzer.Application.Common.Prompts.Models;
+using Feedback.Analyzer.Domain.Common.Events;
+using Feedback.Analyzer.Domain.Entities;
+
+namespace Feedback.Analyzer.Application.Common.Prompts.Events;
+
+/// <summary>
+/// Represents the prompt execution hook event that is triggered after the prompt execution.
+/// </summary>
+/// <typeparam name="TWorkflow"></typeparam>
+public record AfterPromptExecutionEvent<TWorkflow> : Event where TWorkflow : WorkflowContext
+{
+    /// <summary>
+    /// Gets execution context
+    /// </summary>
+    public WorkflowContext Context { get; init; } = default!;
+
+    /// <summary>
+    /// Gets prompt that was executed
+    /// </summary>
+    public AnalysisPrompt Prompt { get; init; } = default!;
+}
