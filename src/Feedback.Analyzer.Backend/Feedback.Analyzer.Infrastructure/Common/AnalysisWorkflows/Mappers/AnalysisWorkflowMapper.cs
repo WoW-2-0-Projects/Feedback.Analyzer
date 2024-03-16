5ab392b0a4c2381606fea38c3bcc2aa988f06c81
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-16 12:07:07 +0500

    Add feedback analysis query and query handlers, mappers and dto models

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/Common/AnalysisWorkflows/Mappers/AnalysisWorkflowMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Models;
+using Feedback.Analyzer.Domain.Entities;
+
+namespace Feedback.Analyzer.Infrastructure.Common.AnalysisWorkflows.Mappers;
+
+public class AnalysisWorkflowMapper : Profile
+{
+    public AnalysisWorkflowMapper()
+    {
+        CreateMap<AnalysisWorkflow, FeedbackAnalysisWorkflowDto>().ReverseMap();
+    }
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisWorkflows/QueryHandlers/FeedbackAnalysisWorkflowGetQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Models;
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Queries;
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Services;
+using Feedback.Analyzer.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisWorkflows.QueryHandlers;
+
+/// <summary>
+/// Represents a query handler for retrieving feedback analysis workflows.
+/// </summary>
+public class FeedbackAnalysisWorkflowGetQueryHandler(IMapper mapper, IFeedbackAnalysisWorkflowService feedbackAnalysisWorkflowService)
+    : IQueryHandler<FeedbackAnalysisWorkflowGetQuery, ICollection<FeedbackAnalysisWorkflowDto>>
+{
+    public async Task<ICollection<FeedbackAnalysisWorkflowDto>> Handle(FeedbackAnalysisWorkflowGetQuery request, CancellationToken cancellationToken)
+    {
+        var matchedFeedbackAnalysisWorkflows = await feedbackAnalysisWorkflowService.Get(
+            request.Filter,
+            new QueryOptions
+            {
+                TrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .Include(workflow => workflow.AnalysisWorkflow)
+            .ToListAsync(cancellationToken);
+        
+        return mapper.Map<ICollection<FeedbackAnalysisWorkflowDto>>(matchedFeedbackAnalysisWorkflows);
+    }
+}
