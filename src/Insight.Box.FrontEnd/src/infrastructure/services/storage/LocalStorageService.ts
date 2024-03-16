59fb84b70f55b6fe4e2093314f76f690c3e5f681
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-16 15:35:21 +0500

    Add docs this service

--- src/Insight.Box.FrontEnd/src/infrastructure/services/storage/LocalStorageService.ts
+++ src/Insight.Box.FrontEnd/src/infrastructure/services/storage/LocalStorageService.ts
@@ -1,18 +1,35 @@
-export  class LocalStorageService {
+/**
+ * LocalStorageService class provides utility methods for interacting with the browser's localStorage.
+ * It allows setting, getting, removing, and clearing data stored in the localStorage.
+ */
+export class localStorageService {
+    /**
+     * Sets a value in the localStorage.
+     */
     public Set<T>(key: string, value: T): void {
         localStorage.setItem(key, JSON.stringify(value));
     }
 
+    /**
+     * Retrieves a value from the localStorage.
+     */
     public get<T>(key: string): T | null {
         const item = localStorage.getItem(key);
         return item ? JSON.parse(item) as T : null;
     }
 
-    public  remove(key: string): void {
+    /**
+     * Removes a value from the localStorage.
+     * @param {string} key - The key of the value to remove.
+     */
+    public remove(key: string): void {
         localStorage.removeItem(key);
     }
 
-    public  clear(): void {
+    /**
+     * Clears all data stored in the localStorage.
+     */
+    public clear(): void {
         localStorage.clear();
     }
 }
--- src/Insight.Box.FrontEnd/src/modules/accounts/services/AuthenticationService.ts
+++ src/Insight.Box.FrontEnd/src/modules/accounts/services/AuthenticationService.ts
@@ -1,46 +1,68 @@
 import {InsightBoxApiClient} from "@/infrastructure/apiClients/insightBoxClient/brokers/InsightBoxApiClient";
-import {LocalStorageService} from "@/infrastructure/services/storage/LocalStorageService";
+import {localStorageService} from "@/infrastructure/services/storage/LocalStorageService";
 import {useAccountStore} from "@/infrastructure/stores/AccountStore";
 import {Account} from "@/modules/accounts/models/Account";
 import type {SignInDetails} from "@/modules/accounts/models/SignInDetails";
-import {Client} from "@/modules/accounts/models/Client";
 
+/**
+ * AuthenticationService class handles authentication-related functionalities such as checking access token,
+ * user login status, signing in, and setting the current user.
+ */
 export class AuthenticationService {
-    private insightBoxApiClient: InsightBoxApiClient;
-    private localStorageService: LocalStorageService;
-    private accountStore = useAccountStore()
+    private insightBoxApiClient: InsightBoxApiClient; // Instance of InsightBoxApiClient for API interactions
+    private localStorageService: localStorageService; // Instance of LocalStorageService for storing tokens
+    private accountStore = useAccountStore(); // Instance of account store for managing user account data
 
+    /**
+     * Constructor for AuthenticationService class.
+     * Initializes InsightBoxApiClient and LocalStorageService instances.
+     */
     constructor() {
         this.insightBoxApiClient = new InsightBoxApiClient();
-        this.localStorageService = new LocalStorageService();
+        this.localStorageService = new localStorageService();
     }
 
+    /**
+     * Checks if an access token exists in the local storage.
+     * @returns {boolean} True if an access token exists, otherwise false.
+     */
     public hasAccessToken() {
         return this.localStorageService.get('accessToken') !== null;
     }
 
-    public  isLoggedIn() {
+    /**
+     * Checks if the user is logged in.
+     * @returns {boolean} True if the user is logged in, otherwise false.
+     */
+    public isLoggedIn() {
         return this.accountStore.account.isLoggedIn();
     }
 
-    public  async signInAsync(signInDetails: SignInDetails) {
+    /**
+     * Attempts to sign in the user asynchronously.
+     * @param {SignInDetails} signInDetails - The sign-in details.
+     * @returns {Promise<boolean>} A promise that resolves to true if sign-in is successful, otherwise false.
+     */
+    public async signInAsync(signInDetails: SignInDetails) {
         const signInResponse = await this.insightBoxApiClient.auth.signInAsync(signInDetails);
 
         if(!signInResponse.isSuccess)
             return false;
 
         // Store security tokens
-        this.localStorageService.Set('accessToken', signInResponse.response?.accessToken)
-        this.localStorageService.Set('refreshToken', signInResponse.response?.refreshToken)
+        this.localStorageService.Set('accessToken', signInResponse.response?.accessToken);
+        this.localStorageService.Set('refreshToken', signInResponse.response?.refreshToken);
 
-        await this.setCurrentUser()
+        await this.setCurrentUser();
 
         return true;
     }
 
-
+    /**
+     * Sets the current user after successful sign-in.
+     */
     public async setCurrentUser() {
-        if(this.isLoggedIn() || !this.hasAccessToken()) return
+        if(this.isLoggedIn() || !this.hasAccessToken()) return;
 
         const currentUser = await this.insightBoxApiClient.auth.getCurrentUser();
         if(currentUser.isSuccess){
