025817afc7b9223b2249a3cf71c0ed0922778180
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-03-17 14:39:55 +0500

    Add status property to Analysis workflow migration

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Migrations/20240317093428_Add_Status_Property_To_Workflow_Analysis.Designer.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Migrations/20240317093428_Add_Status_Property_To_Workflow_Analysis.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Feedback.Analyzer.Persistence.DataContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Feedback.Analyzer.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240317093428_Add_Status_Property_To_Workflow_Analysis")]
+    partial class Add_Status_Property_To_Workflow_Analysis
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
