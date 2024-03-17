47c1e86cab50f9046530a8a16379d37391ce1dc6
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-17 14:09:46 +0500

    Add workflow tab and create, update endpoints client

--- src/Insight.Box.FrontEnd/src/infrastructure/apiClients/insightBoxClient/brokers/WorkflowsEndpointsClient.ts
+++ src/Insight.Box.FrontEnd/src/infrastructure/apiClients/insightBoxClient/brokers/WorkflowsEndpointsClient.ts
@@ -3,6 +3,12 @@ import type {Query} from "@/infrastructure/models/query/Query";
 import type {RequestFormatterService} from "@/infrastructure/apiClients/formatters/RequestFormatterService";
 import type {FeedbackAnalysisWorkflow} from "@/modules/prompts/models/FeedbackAnalysisWorkflow";
 import type {FeedbackAnalysisWorkflowFilter} from "@/modules/workflows/models/FeedbackAnalysisWorkflowFilter";
+import type {
+    CreateFeedbackAnalysisWorkflowCommand
+} from "@/modules/workflows/models/CreateFeedbackAnalysisWorkflowCommand";
+import type {
+    UpdateFeedbackAnalysisWorkflowCommand
+} from "@/modules/workflows/models/UpdateFeedbackAnalysisWorkflowCommand";
 
 export class WorkflowsEndpointsClient {
     public client: ApiClientBase;
@@ -16,6 +22,16 @@ export class WorkflowsEndpointsClient {
     public async getAsync(query: Query<FeedbackAnalysisWorkflowFilter>) {
         const endpointUrl = this.requestFormatterService.addQueryParams('api/workflows', query);
         return await this.client.getAsync<Array<FeedbackAnalysisWorkflow>>(endpointUrl);
+    }
+
+    public async createAsync(command: CreateFeedbackAnalysisWorkflowCommand) {
+        const endpointUrl =  'api/workflows';
+        return await this.client.postAsync<FeedbackAnalysisWorkflow>(endpointUrl, command);
+    }
+
+    public async updateAsync(command: UpdateFeedbackAnalysisWorkflowCommand) {
+        const endpointUrl =  'api/workflows';
+        return await this.client.putAsync<FeedbackAnalysisWorkflow>(endpointUrl, command);
     }
 
     public async executeSinglePromptAsync(workflowId: string, promptId: string) {
