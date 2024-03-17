22d96d1fe16503caa250e97be8361affe016845a
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-03-17 12:20:19 +0500

    created user created event and its handler

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Identity/Events/UserCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using Feedback.Analyzer.Domain.Common.Events;
+
+namespace Feedback.Analyzer.Application.Identity.Events;
+
+/// <summary>
+/// Represents user created event
+/// </summary>
+public record UserCreatedEvent : EventBase
+{
+}
---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/Identity/EventHandlers/UserCreatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using Feedback.Analyzer.Application.Identity.Events;
+using Feedback.Analyzer.Domain.Common.Events;
+
+namespace Feedback.Analyzer.Infrastructure.Identity.EventHandlers;
+
+/// <summary>
+///  Represents event handler for user created event
+/// </summary>
+public class UserCreatedEventHandler : EventHandlerBase<UserCreatedEvent>
+{
+    protected override ValueTask HandleAsync(UserCreatedEvent @event, CancellationToken cancellationToken)
+    {
+        return ValueTask.CompletedTask;
+    }
+}
