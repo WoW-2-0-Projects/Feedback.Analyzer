4d0645a2ea7505cc83e2b2737eb65b319968549c
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-17 18:10:20 +0500

    remove old client migration and added added client migration

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Migrations/20240317120019_Add  Client Entity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Feedback.Analyzer.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Clients",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "Client");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Clients");
+        }
+    }
+}
