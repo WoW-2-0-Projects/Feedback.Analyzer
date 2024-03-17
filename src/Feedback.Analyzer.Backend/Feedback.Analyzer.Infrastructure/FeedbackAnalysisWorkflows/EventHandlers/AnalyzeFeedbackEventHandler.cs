fa58c9b6daca3b418f3fe943029adfaf755134df
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-03-17 14:41:01 +0500

    Add Analyze feedback event and event handler

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisWorkflows/EventHandlers/AnalyzeFeedbackEventHandler.cs
@@ -0,0 +1,17 @@
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Events;
+using Feedback.Analyzer.Application.FeedbackAnalysisWorkflows.Services;
+using Feedback.Analyzer.Domain.Common.Events;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Feedback.Analyzer.Infrastructure.FeedbackAnalysisWorkflows.EventHandlers;
+
+public class AnalyzeFeedbackEventHandler(IServiceScopeFactory serviceScopeFactory) : IEventHandler<AnalyzeFeedbackEvent>
+{
+    public async Task Handle(AnalyzeFeedbackEvent notification, CancellationToken cancellationToken)
+    {
+        var scopedServiceProvider = serviceScopeFactory.CreateScope().ServiceProvider;
+        var feedbackAnalysisOrchestrationService = scopedServiceProvider.GetRequiredService<IFeedbackAnalysisOrchestrationService>();
+
+        await feedbackAnalysisOrchestrationService.ExecuteWorkflowAsync(notification.Context, cancellationToken);
+    }
+}
