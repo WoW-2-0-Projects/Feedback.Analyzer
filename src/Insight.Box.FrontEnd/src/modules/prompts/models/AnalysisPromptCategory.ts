9ffd2272d7fa451ecb254ca14783b7e721705fd3
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-19 17:55:46 +0500

    added all necessary  model for prompts endpoint client

---
+++ src/Insight.Box.FrontEnd/src/modules/prompts/models/AnalysisPromptCategory.ts
@@ -0,0 +1,42 @@
+import type {FeedbackAnalysisPromptType} from "@/modules/prompts/models/FeedbackAnalysisPromptType";
+import type {AnalysisPrompt} from "@/modules/prompts/models/AnalysisPrompt";
+
+/**
+ * Represents a category for analysis prompts.
+ */
+export class AnalysisPromptCategory {
+    /*
+    * The ID of the category.
+    */
+    id!: string;
+
+    /*
+    * The type of feedback analysis prompt associated with the category.
+    */
+    type!: FeedbackAnalysisPromptType;
+
+    /*
+    * The display name of the feedback analysis prompt type.
+    */
+    typeDisplayName!: string;
+
+    /*
+    * The ID of the selected prompt within the category.
+    */
+    selectedPromptId!: string;
+
+    /*
+    * The version of the selected prompt within the category.
+    */
+    selectedPromptVersion!: string;
+
+    /*
+    * The total number of prompts within the category.
+    */
+    promptsCount!: number;
+
+    /*
+    * An array containing the prompts within the category.
+    */
+    prompts!: Array<AnalysisPrompt>;
+}
---
+++ src/Insight.Box.FrontEnd/src/modules/prompts/models/FeedbackAnalysisPromptType.ts
@@ -0,0 +1,29 @@
+/**
+ * Enum representing different types of feedback analysis prompts.
+ */
+export enum FeedbackAnalysisPromptType {
+    /*
+     * Prompt type for content safety analysis.
+     */
+    ContentSafetyAnalysis,
+
+    /*
+    * Prompt type for language recognition.
+    */
+    RecognizeLanguages,
+
+    /*
+    * Prompt type for relevance analysis.
+    */
+    RelevanceAnalysis,
+
+    /*
+    * Prompt type for extracting relevant content.
+    */
+    ExtractRelevantContent,
+
+    /*
+     * Prompt type for redacting personal information.
+     */
+    RedactPersonalInformation
+}
