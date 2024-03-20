fbbe2017ef10d96873713cab989164faced46d67
Ilxom <ilxomkarimjonov22@gmail.com>
2024-03-20 15:53:11 +0500

    Enhance After Prompt Execution Event Handler

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisWorkflows/EventHandlers/AfterPromptExecutionEventHandler.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/FeedbackAnalysisWorkflows/EventHandlers/AfterPromptExecutionEventHandler.cs
@@ -18,50 +18,45 @@ public class AfterPromptExecutionEventHandler : IEventHandler<AfterPromptExecuti
         if (notification.Context is not SingleFeedbackAnalysisWorkflowContext context)
             return Task.CompletedTask;
         
+        var lastHistory = context.GetLastHistory(notification.Prompt.CategoryId);
+
+        if (lastHistory is null)
+        {
+            context.Status = WorkflowStatus.Failed;
+            context.ErrorMessage = $"No history found for the category - {notification.Prompt.Category.Category.GetDisplayName()}";
+
+            return Task.CompletedTask;
+        }
+
+        if (lastHistory.Result is null)
+        {
+            context.Status = WorkflowStatus.Failed;
+            context.ErrorMessage =
+                $"Result of the last history is null for the category - {notification.Prompt.Category.Category.GetDisplayName()} is null";
+
+            return Task.CompletedTask;
+        }
+        
         switch (notification.Prompt.Category.Category)
         {
             case FeedbackAnalysisPromptCategory.RelevanceAnalysis:
             {
-                var lastHistory = context.GetLastHistory(notification.Prompt.CategoryId) ?? throw new InvalidOperationException(
-                    $"No history found for the category - {notification.Prompt.Category.Category.GetDisplayName()}"
-                );
-
-                if (lastHistory.Result is null)
-                    throw new InvalidOperationException(
-                        $"Result of the last history is null for the category - {notification.Prompt.Category.Category.GetDisplayName()} is null"
-                    );
-
                 context.Result.FeedbackRelevance.IsRelevant = JsonConvert.DeserializeObject<bool>(lastHistory.Result!.ToLower());
 
                 if (!context.Result.FeedbackRelevance.IsRelevant)
-                    throw new InvalidOperationException("Feedback is not relevant");
+                {
+                    context.Status = WorkflowStatus.Completed;
+                    context.ErrorMessage = "Feedback is not relevant";
+                }
                 break;
             }
             case FeedbackAnalysisPromptCategory.RelevantContentExtraction:
             {
-                var lastHistory = context.GetLastHistory(notification.Prompt.CategoryId) ?? throw new InvalidOperationException(
-                    $"No history found for the category - {notification.Prompt.Category.Category.GetDisplayName()}"
-                );
-
-                if (lastHistory.Result is null)
-                    throw new InvalidOperationException(
-                        $"Result of the last history is null for the category - {notification.Prompt.Category.Category.GetDisplayName()} is null"
-                    );
-
                 context.Result.FeedbackRelevance.ExtractedRelevantContent = lastHistory.Result;
                 break;
             }
             case FeedbackAnalysisPromptCategory.PersonalInformationRedaction:
             {
-                var lastHistory = context.GetLastHistory(notification.Prompt.CategoryId) ?? throw new InvalidOperationException(
-                    $"No history found for the category - {notification.Prompt.Category.Category.GetDisplayName()}"
-                );
-
-                if (lastHistory.Result is null)
-                    throw new InvalidOperationException(
-                        $"Result of the last history is null for the category - {notification.Prompt.Category.Category.GetDisplayName()} is null"
-                    );
-
                 context.Result.FeedbackRelevance.PiiRedactedContent = lastHistory.Result;
                 break;
             }
