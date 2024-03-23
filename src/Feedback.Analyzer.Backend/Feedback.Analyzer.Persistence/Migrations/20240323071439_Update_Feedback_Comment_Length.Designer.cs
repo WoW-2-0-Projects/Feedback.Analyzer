0d5fbf102e38c6ac24c76bc69233193d5362a825
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-23 12:18:03 +0500

    Add update customer feedback max length migration

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Migrations/20240323071439_Update_Feedback_Comment_Length.Designer.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Migrations/20240323071439_Update_Feedback_Comment_Length.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Feedback.Analyzer.Persistence.DataContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Feedback.Analyzer.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240323071439_Update_Feedback_Comment_Length")]
+    partial class Update_Feedback_Comment_Length
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
