9b4db8a0c650b871db912060ec8cd2685cc6d54d
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-03-28 10:47:04 +0500

    Adede update selectef prompt command and prpmpts controller

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Application/Common/PromptCategory/Commands/UpdateSelectedPromptCommand.cs
@@ -0,0 +1,19 @@
+﻿using Feedback.Analyzer.Domain.Common.Commands;
+
+namespace Feedback.Analyzer.Application.Common.PromptCategory.Commands;
+
+/// <summary>
+/// Represents a command for updating the selected prompt.
+/// </summary>
+public class UpdateSelectedPromptCommand : ICommand< bool>
+{
+    /// <summary>
+    /// Gets or sets the ID of the category to which the prompt belongs.
+    /// </summary>
+    public Guid CategoryId { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the ID of the prompt to be selected.
+    /// </summary>
+    public Guid PromptId { get; set; }
+}
