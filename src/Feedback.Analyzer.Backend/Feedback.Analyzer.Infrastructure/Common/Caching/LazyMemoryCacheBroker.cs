d28bd1160a5e744d57abbf4977a949d54c49b57a
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-29 05:34:39 +0500

    Install lazy cache and update caching infrastructure

---
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Infrastructure/Common/Caching/LazyMemoryCacheBroker.cs
@@ -0,0 +1,117 @@
+﻿using Force.DeepCloner;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace Feedback.Analyzer.Infrastructure.Common.Caching;
+
+/// <summary>
+/// Provides caching functionalities using an in-memory cache.
+/// </summary>
+public class LazyMemoryCacheBroker(IOptions<CacheSettings> cacheSettings, IAppCache memoryCache) : ICacheBroker
+{
+    private readonly MemoryCacheEntryOptions _memoryCacheEntryOptions = new()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(cacheSettings.Value.AbsoluteExpirationInSeconds),
+        SlidingExpiration = TimeSpan.FromSeconds(cacheSettings.Value.SlidingExpirationInSeconds)
+    };
+
+    public ValueTask<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default)
+    {
+        return ValueTask.FromResult(memoryCache.Get<T?>(key));
+    }
+
+    public ValueTask<bool> TryGetAsync<T>(string key, out T? value, CancellationToken cancellationToken = default)
+    {
+        value = memoryCache.Get<T?>(key);
+
+        return value is not null ? ValueTask.FromResult(true) : ValueTask.FromResult(false);
+    }
+
+    public async ValueTask<T?> GetOrSetAsync<T>(
+        string key,
+        T value,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var cachedValue = memoryCache.Get<T?>(key);
+
+        if (cachedValue is not null)
+            return cachedValue;
+
+        await SetAsync(key, value, entryOptions, cancellationToken);
+
+        return value;
+    }
+
+    public ValueTask<T?> GetOrSetAsync<T>(
+        string key,
+        Func<T> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return new ValueTask<T?>(memoryCache.GetOrAdd(key, valueProvider, GetCacheEntryOptions(entryOptions)));
+    }
+
+    public async ValueTask<T?> GetOrSetAsync<T>(
+        string key,
+        Func<ValueTask<T>> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return await memoryCache.GetOrAddAsync(key, async () => await valueProvider(), GetCacheEntryOptions(entryOptions));
+    }
+
+    public ValueTask SetAsync<T>(string key, T value, CacheEntryOptions? entryOptions = default, CancellationToken cancellationToken = default)
+    {
+        memoryCache.Add(key, value, GetCacheEntryOptions(entryOptions));
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask SetAsync<T>(
+        string key,
+        Func<T> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        memoryCache.Add(key, valueProvider(), GetCacheEntryOptions(entryOptions));
+        return ValueTask.CompletedTask;
+    }
+
+    public async ValueTask SetAsync<T>(
+        string key,
+        Func<ValueTask<T>> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        memoryCache.Add(key, await valueProvider(), GetCacheEntryOptions(entryOptions));
+    }
+
+    public ValueTask DeleteAsync(string key, CancellationToken cancellationToken = default)
+    {
+        memoryCache.Remove(key);
+        return ValueTask.CompletedTask;
+    }
+
+    /// <summary>
+    /// Gets the cache entry options based on given entry options or default options.
+    /// </summary>
+    /// <param name="entryOptions">Given cache entry options.</param>
+    /// <returns>The memory cache entry options.</returns>
+    private MemoryCacheEntryOptions GetCacheEntryOptions(CacheEntryOptions? entryOptions)
+    {
+        if (!entryOptions.HasValue ||
+            (!entryOptions.Value.AbsoluteExpirationRelativeToNow.HasValue && !entryOptions.Value.SlidingExpiration.HasValue))
+            return _memoryCacheEntryOptions;
+
+        var currentEntryOptions = _memoryCacheEntryOptions.DeepClone();
+        currentEntryOptions.AbsoluteExpirationRelativeToNow = entryOptions.Value.AbsoluteExpirationRelativeToNow;
+        currentEntryOptions.SlidingExpiration = entryOptions.Value.SlidingExpiration;
+
+        return currentEntryOptions;
+    }
+}
--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Caching/Brokers/ICacheBroker.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Persistence/Caching/Brokers/ICacheBroker.cs
@@ -1,62 +1,132 @@
 ﻿using Feedback.Analyzer.Persistence.Caching.Models;
 
 namespace Feedback.Analyzer.Persistence.Caching.Brokers;
+
 /// <summary>
-/// Represents an interface for a cache broker that provides methods for interacting with a caching mechanism.
+/// Defines cache broker functionality.
 /// </summary>
 public interface ICacheBroker
 {
-   /// <summary>
-   /// Asynchronously retrieves a cached item with the specified key.
-   /// </summary>
-   /// <param name="key"></param>
-   /// <param name="cancellationToken"></param>
-   /// <typeparam name="T"></typeparam>
-   /// <returns></returns>
+    /// <summary>
+    /// Gets the cache entry value with the specified key.
+    /// </summary>
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Found instance of <see cref="T"/> if found, otherwise default value.</returns>
     ValueTask<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Attempts to asynchronously retrieve a cached item with the specified key.
+    /// Attempts to get the cache entry with the specified key.
     /// </summary>
-    /// <param name="key"></param>
-    /// <param name="value"></param>
-    /// <param name="cancellationToken"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="value">Cached entry value if found, otherwise default value</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>True if the cached entry was found and retrieved successfully; otherwise, false.</returns>
     ValueTask<bool> TryGetAsync<T>(string key, out T? value, CancellationToken cancellationToken = default);
-
+    
     /// <summary>
-    ///Asynchronously retrieves a cached item with the specified key, or sets the item if it does not exist.
+    /// Attempts to get the cache entry with the specified key. If not found, sets the cache entry with the specified key and value.
     /// </summary>
-    /// <param name="key"></param>
-    /// <param name="valueFactory"></param>
-    /// <param name="entryOptions"></param>
-    /// <param name="cancellationToken"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="value">Value to create cache entry for</param>
+    /// <param name="entryOptions">A cache entry options to specify caching options</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Found instance of <see cref="T"/> if found, otherwise default value.</returns>
     ValueTask<T?> GetOrSetAsync<T>(
         string key,
-        Func<Task<T>> valueFactory,
+        T value,
         CacheEntryOptions? entryOptions = default,
         CancellationToken cancellationToken = default
     );
 
     /// <summary>
-    /// Asynchronously sets a cached item with the specified key and value.
+    /// Attempts to get the cache entry with the specified key. If not found, sets the cache entry with the specified key and value.
     /// </summary>
-    /// <param name="key"></param>
-    /// <param name="value"></param>
-    /// <param name="entryOptions"></param>
-    /// <param name="cancellationToken"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    ValueTask SetAsync<T>(string key, T value, CacheEntryOptions? entryOptions = default, CancellationToken cancellationToken = default);
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="valueProvider">A broker that provides the entry to set</param>
+    /// <param name="entryOptions">A cache entry options to specify caching options</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Found instance of <see cref="T"/> if found, otherwise default value.</returns>
+    ValueTask<T?> GetOrSetAsync<T>(
+        string key,
+        Func<T> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    );
+    
+    /// <summary>
+    /// Attempts to get the cache entry with the specified key. If not found, sets the cache entry with the specified key and value.
+    /// </summary>
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="valueProvider">A broker that provides the entry to set</param>
+    /// <param name="entryOptions">A cache entry options to specify caching options</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Found instance of <see cref="T"/> if found, otherwise default value.</returns>
+    ValueTask<T?> GetOrSetAsync<T>(
+        string key,
+        Func<ValueTask<T>> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    );
+    
+    /// <summary>
+    /// Sets the cache entry with the specified key and value.
+    /// </summary>
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="value">Value to create cache entry for</param>
+    /// <param name="entryOptions">A cache entry options to specify caching options</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Found instance of <see cref="T"/> if found, otherwise default value.</returns>
+    ValueTask SetAsync<T>(
+        string key,
+        T value,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    );
 
     /// <summary>
-    /// Asynchronously deletes a cached item with the specified key.
+    /// Sets the cache entry with the specified key and value.
     /// </summary>
-    /// <param name="key"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="valueProvider">A broker that provides the entry to set</param>
+    /// <param name="entryOptions">A cache entry options to specify caching options</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Found instance of <see cref="T"/> if found, otherwise default value.</returns>
+    ValueTask SetAsync<T>(
+        string key,
+        Func<T> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    );
+    
+    /// <summary>
+    /// Sets the cache entry with the specified key and value.
+    /// </summary>
+    /// <typeparam name="T">The type of the cache entry value.</typeparam>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="valueProvider">A broker that provides the entry to set</param>
+    /// <param name="entryOptions">A cache entry options to specify caching options</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Found instance of <see cref="T"/> if found, otherwise default value.</returns>
+    ValueTask SetAsync<T>(
+        string key,
+        Func<ValueTask<T>> valueProvider,
+        CacheEntryOptions? entryOptions = default,
+        CancellationToken cancellationToken = default
+    );
+
+    /// <summary>
+    /// Deletes the cache entry with the specified key.
+    /// </summary>
+    /// <param name="key">The key of the cache entry.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
     ValueTask DeleteAsync(string key, CancellationToken cancellationToken = default);
 }
