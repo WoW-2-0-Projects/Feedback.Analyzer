f29239956d04997addba2e6522a48c03afbe1c79
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-03-29 10:56:53 +0500

    Add processed and failed feedbacks count to feedback analysis workflow result

--- src/Feedback.Analyzer.Backend/Feedback.Analyzer.Domain/Entities/FeedbackAnalysisWorkflowResult.cs
+++ src/Feedback.Analyzer.Backend/Feedback.Analyzer.Domain/Entities/FeedbackAnalysisWorkflowResult.cs
@@ -11,26 +11,36 @@ public class FeedbackAnalysisWorkflowResult : Entity
     /// Gets or sets the ID of the associated workflow.
     /// </summary>
     public Guid WorkflowId { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the count of feedbacks analyzed in the workflow.
     /// </summary>
-    public ulong FeedbacksCount { get; set; }
-    
+    public uint FeedbacksCount { get; set; }
+
     /// <summary>
     /// Gets or sets the start time of the workflow.
     /// </summary>
     public DateTimeOffset StartedTime { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the finish time of the workflow.
     /// </summary>
     public DateTimeOffset? FinishedTime { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the associated feedback analysis workflow.
     /// </summary>
     public FeedbackAnalysisWorkflow Workflow { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the count of feedbacks processed in the workflow.
+    /// </summary>
+    public uint ProcessedFeedbacksCount { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the count of feedbacks successfully processed in the workflow.
+    /// </summary>
+    public uint FailedFeedbacksCount { get; set; } = default!;
 
     /// <summary>
     /// Gets or sets the collection of feedback analysis results associated with this workflow result.
